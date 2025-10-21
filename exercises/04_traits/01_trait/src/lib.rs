5f4f55c92394e5da9bec5ec0ecc88659cdc64d5f
Magnus Markling <magnus@markling.com>
2025-10-21 19:07:10 +0100

    finish 04_traits
    Add<&u16> is never verified

--- exercises/04_traits/01_trait/src/lib.rs
+++ exercises/04_traits/01_trait/src/lib.rs
@@ -3,6 +3,22 @@
 //
 // Then implement the trait for `u32` and `i32`.
 
+pub trait IsEven {
+    fn is_even(self) -> bool;
+}
+
+impl IsEven for u32 {
+    fn is_even(self) -> bool {
+        self.is_multiple_of(2)
+    }
+}
+
+impl IsEven for i32 {
+    fn is_even(self) -> bool {
+        self % 2 == 0
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
